434ae1ab2f303321d6600444c4b28179fe9f5760
Adham <adhamxudayberganov942@gmail.com>
2026-09-10 19:57:46 +0500

    contact tab was added

--- routes/tabs.route.js
+++ routes/tabs.route.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET tabs listing. */
 router.get('/skills', (req, res) => {
-  res.render('skills');
+  res.locals.title = 'Technical Skills'
+  res.render('skills',);
 });
 
 router.get('/experience', (req, res) => {
-  res.render('experience');
+  res.render('experience', {title: 'My Experiences'});
 });
 
 router.get('/projects', (req, res) => {
-  res.render('projects');
+  res.render('projects', {title: 'Projects That I built'});
 });
 
 router.get('/education', (req, res) => {
-  res.render('education');
+  res.render('education', {title: 'Academic Path'});
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', {title: 'Contact With Me'});
 });
 
 module.exports = router;
